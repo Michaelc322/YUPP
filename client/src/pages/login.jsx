0496afca02c4ba4648f4947b4c044e7d8ef41611
TannerR20 <tannerrohloff@hotmail.com>
2024-10-11 01:40:24 -0400

    login page

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const login = () => {
+  return (
+    <div>
+        <p1>Login:</p1>
+        <input type="text">Username</input>
+        <input type="text">Password</input>
+        <button type="button">Login</button>
+    </div>
+  )
+}
+
+export default login
