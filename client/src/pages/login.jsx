b77904080dda68e4d89472941f32aab30d971424
TannerR20 <tannerrohloff@hotmail.com>
2024-10-11 01:40:59 -0400

    login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,13 +1,22 @@
+<form action = "action page.php" method ="post">
+
+</form>
 import React from 'react'
 
 
 const login = () => {
   return (
-    <div>
-        <p1>Login:</p1>
-        <input type="text">Username</input>
-        <input type="text">Password</input>
-        <button type="button">Login</button>
+    <div class = "container">
+        <br></br>
+        <form action = "POST" class="form" id="login">
+          <h1 class="form_title">Login:</h1>
+          <div class="form_message form__message--error">Incorrect username/password.</div>
+          <label for="uname">Username:</label><br></br>
+          <input type="text" id="uname" name="uname"></input><br></br>
+          <label for="pass">Password:</label><br></br>
+          <input type="text" id="lname" name="lname"></input><br></br>
+          <button type="submit">Login</button>
+        </form>
     </div>
   )
 }
